8821a8c171df2e8fa054f3607e245c56a15b3920
rubik <20113674@setu.ie>
2026-04-24 23:50:51 +0100

    stats

--- routes.js
+++ routes.js
@@ -11,7 +11,7 @@ import start from './controllers/start.js';
 import dashboard from './controllers/dashboard.js';
 import about from './controllers/about.js'
 import playlist from './controllers/playlist.js';
-
+import stats from './controllers/stats.js';
 router.get('/', start.createView);
 router.get('/dashboard', dashboard.createView);
 router.get('/about', about.createView);
@@ -28,5 +28,7 @@ router.get('/playlist/:id/deletesong/:songid', playlist.deleteSong);
 router.get('/dashboard/deleteplaylist/:id', dashboard.deletePlaylist);
 
 router.post('/playlist/:id/updatesong/:songid', playlist.updateSong);
+
+router.get('/stats', stats.createView);
 
 export default router;
