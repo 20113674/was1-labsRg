531c14edf749090ff5ce8ef4e980ad480172b800
rubik <20113674@setu.ie>
2026-05-03 19:17:27 +0100

    cookies user store accounts routes

--- routes.js
+++ routes.js
@@ -12,7 +12,12 @@ import dashboard from './controllers/dashboard.js';
 import about from './controllers/about.js'
 import playlist from './controllers/playlist.js';
 import stats from './controllers/stats.js';
-router.get('/', start.createView);
+
+import accounts from './controllers/accounts.js';
+
+
+router.get('/start', start.createView);
+
 router.get('/dashboard', dashboard.createView);
 router.get('/about', about.createView);
 router.get('/playlist/:id', playlist.createView);
@@ -36,3 +41,10 @@ export default router;
 router.get('/searchCategory', dashboard.createView);
 
 router.get('/sortData', dashboard.createView);
+
+router.get('/', accounts.index);
+router.get('/login', accounts.login);
+router.get('/signup', accounts.signup);
+router.get('/logout', accounts.logout);
+router.post('/register', accounts.register);
+router.post('/authenticate', accounts.authenticate);
